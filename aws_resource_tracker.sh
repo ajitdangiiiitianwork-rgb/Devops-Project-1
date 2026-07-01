158d7f32c57f4022fc9665cb1c753658b0338966
Ubuntu <ubuntu@ip-172-31-36-82.ap-south-1.compute.internal>
2026-07-01 07:43:33 +0000

    Added Print statements

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -14,13 +14,17 @@
 # AWS IAM Users
 
 # List S3 buckets
+echo "Print list of S3 buckets"
 aws s3 ls
 
 # List EC-2 instances
+echo "Print list of EC-2 instances"
 aws ec2 describe-instances
 
 # List the lambda functions
+echo "Print list of Lambda functions"
 aws lambda list-functions
 
 # List IAM users
+echo "Print list of IAM users"
 aws iam list-users
